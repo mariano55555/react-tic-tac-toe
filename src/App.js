909a4addb6e4b5aac8ff442f730c88ba2d1c571e
Mariano Paz <jorge.sv@squeaky.com>
2018-01-04 08:16:38 -0600

    - Initial setup (Components setup) - Clean code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Square from './Square';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      boardsize: 3,
+      gameBoard:{},
+      canvas: Array.apply(null, Array(3)),
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <div className="tic-tac-toe">
+         { this.state.canvas.map(function(value, row){
+            return (
+               this.state.canvas.map(function(val, column){
+                return(
+                  <Square
+                  key={row+''+column}
+                  row={row}
+                  column={column}
+                  gameBoard={this.state.gameBoard}
+                  value={this.state.gameBoard[row+''+column]}
+                  boardsize={this.state.boardsize}
+                  />
+                )
+              }.bind(this))
+            )
+          }.bind(this))}
+        </div>
       </div>
     );
   }
---
+++ src/Square.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./Square.css";
+
+export default class Square extends Component {
+    tileClick(props){
+        props.updateBoard(props.row, props.column, props.turn);
+    }
+    render() {
+        return (
+            <div className={"tile " + this.props.row+''+this.props.column } onClick={() => this.tileClick(this.props)}>
+                <p>{this.props.gameBoard[this.props.row+''+this.props.column]}</p>
+            </div>
+        )
+    }
+}
